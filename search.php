2ae58a20b6f005d84d8ae2f2a4ed50429517cc09
Remington <remingtonwilliams@gmail.com>
2015-07-19 02:00:05 -0500

    sunday 2am push

---
+++ search.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Search Results</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+<link rel="stylesheet" href="css/bootstrap-responsive.css" type="text/css" media="screen">    
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+
+<script type="text/javascript" src="js/jquery.js"></script>  
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/superfish.js"></script>
+
+<script type="text/javascript" src="js/jquery.ui.totop.js"></script>
+
+<script type="text/javascript" src="search/search.js"></script>
+<script>
+$(document).ready(function() {
+	// 
+
+
+
+}); //
+$(window).load(function() {
+	//
+
+}); //
+</script>		
+<!--[if lt IE 8]>
+		<div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+	<![endif]-->    
+
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>      
+  <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+<![endif]-->
+</head>
+
+<body class="subpage">
+<div id="main">
+
+<div class="top1">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top1_inner clearfix">
+<header><div class="logo_wrapper"><a href="index.html" class="logo"><img src="images/logo.png" alt=""></a></div></header>	
+<div class="phone1"><div class="txt1">Call: <span> 1 800 123 4567</span></div><div class="over1"></div></div>
+</div>	
+</div>	
+</div>	
+</div>	
+</div>
+
+<div class="top2">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top2_inner clearfix">
+<div class="navbar navbar_">
+	<div class="navbar-inner navbar-inner_">
+		<a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</a>
+		<div class="nav-collapse nav-collapse_ collapse">
+			<ul class="nav sf-menu clearfix">
+<li><a href="index.html">Home</a></li>
+<li class="sub-menu sub-menu-1"><a href="index-1.html">Who We Are</a>
+	<ul>
+		<li><a href="index-1.html">about us</a></li>		
+		<li><a href="index-1.html">news & events</a></li>	
+		<li><a href="404.html">404 page</a></li>					
+	</ul>						
+</li>
+<li><a href="index-2.html">Services</a></li>
+<li><a href="index-3.html">Prices</a></li>
+<li><a href="index-4.html">Testimonials</a></li>
+<li><a href="index-5.html">Gallery</a></li>
+<li><a href="index-6.html">Contacts</a></li>											
+<li class="last"></li>											
+    </ul>
+		</div>
+	</div>
+</div>
+</div>	
+</div>	
+</div>	
+</div>	
+</div>
+
+<div class="top3">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top3_inner clearfix">
+
+<div class="breadcrumbs1"><a href="index.html">Home Page</a>&nbsp;&nbsp;&nbsp;<span>></span>&nbsp;&nbsp;&nbsp;Search</div>
+
+</div>	
+</div>	
+</div>	
+</div>	
+</div>
+
+
+
+<div id="content">
+<div class="container">
+	
+<div class="row">
+<div class="span12">
+	
+<h2 class="w1">Search result:<b></b></h2>
+
+<div id="search-results"></div>	
+
+
+</div>	
+</div>
+</div>	
+</div>
+
+<div class="bot1">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="bot1_inner clearfix"></div>	
+
+<div class="banners">
+<div class="row">
+<div class="span3 banner banner1">
+<div class="banner_inner">
+<a href="#">
+<div class="txt">
+	<div class="txt1">Home<br>REMODELING</div>
+	<div class="txt2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod </div>
+</div>		
+<img src="images/banner1.png" alt="" class="img">
+<div class="txt3">read<br>more</div>
+</a>
+<img src="images/shadow1.png" alt="" class="sh1">	
+</div>	
+</div>	
+<div class="span3 banner banner2">
+<div class="banner_inner">
+<a href="#">
+<div class="txt">
+	<div class="txt1">Overall<br>Maintenance<br>& Repairs</div>
+	<div class="txt2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod </div>
+</div>		
+<img src="images/banner2.png" alt="" class="img">
+<div class="txt3">read<br>more</div>
+</a>
+<img src="images/shadow1.png" alt="" class="sh1">	
+</div>	
+</div>
+
+<div class="span3 banner banner3">
+<div class="banner_inner">
+<a href="#">
+<div class="txt">
+	<div class="txt1">Drywall<br>& PAINTING</div>
+	<div class="txt2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod </div>
+</div>		
+<img src="images/banner3.png" alt="" class="img">
+<div class="txt3">read<br>more</div>
+</a>
+<img src="images/shadow1.png" alt="" class="sh1">	
+</div>	
+</div>	
+<div class="span3 banner banner4">
+<div class="banner_inner">
+<a href="#">
+<div class="txt">
+	<div class="txt1">Deck &<br>Carpentry
+</div>
+	<div class="txt2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod </div>
+</div>		
+<img src="images/banner4.png" alt="" class="img">
+<div class="txt3">read<br>more</div>
+</a>
+<img src="images/shadow1.png" alt="" class="sh1">	
+</div>	
+</div>
+
+</div>
+
+</div>
+
+</div>	
+</div>	
+</div>	
+</div>
+
+<div class="bot2">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="bot2_inner clearfix">
+<div class="row">
+<div class="span3">
+<footer>
+<div class="logo2_wrapper"><a href="index.html" class="logo2"><img src="images/logo2.png" alt=""></a></div>	
+<div class="copyright">Copyright   © 2013. All rights reserved. Handyman Service.</div>
+<div class="copyright2"><a href="#">Privacy Policy</a>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;<a href="#">Terms of Use</a></div>
+
+
+</footer>	
+</div>
+<div class="span3">
+<div class="bot2_title">Contacts:</div>	
+<div class="phone2">
+<div class="txt1">Call for more information:</div>	
+<div class="txt2">1 800 123 4567</div>
+</div>
+</div>
+<div class="span3">
+<div class="bot2_title">Get in Touch:</div>	
+<div class="social_wrapper">
+	<ul class="social clearfix">
+	    <li><a href="#"><img src="images/social_ic1.png"></a></li>
+	    <li><a href="#"><img src="images/social_ic2.png"></a></li>
+	    <li><a href="#"><img src="images/social_ic3.png"></a></li>
+	    <li><a href="#"><img src="images/social_ic4.png"></a></li>
+	</ul>
+</div>
+</div>
+<div class="span3">
+<div class="bot2_title">Site search:</div>	
+<div class="search-form-wrapper clearfix">
+<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="navbar-form" >
+						<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
+						<a href="#" onClick="document.getElementById('search-form').submit()"></a>
+					</form>	
+</div>
+</div>	
+</div>
+</div>	
+</div>	
+</div>	
+</div>	
+</div>
+
+
+
+
+	
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
